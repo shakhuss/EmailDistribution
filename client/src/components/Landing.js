b5042d3e747dd52a8a90df363fcf91a25e1a6cda
Shakeb Hussain <shakebhussain786@gmail.com>
2020-04-13 22:56:31 -0500

    Added billing and client side

---
+++ client/src/components/Landing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div style={{textAlign: 'center'}}>
+            <h1>
+                Email Dist.
+            </h1>
+            Collect Feedback From Your Users
+
+        </div>
+    );
+};
+
+export default Landing;
